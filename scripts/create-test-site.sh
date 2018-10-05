ccba8b8884fe9747687619b69114b1518b721953
Alexander O'Neill <aloneill@gmail.com>
2018-10-05 04:12:49 -0300

    Content in review change dot table, filter added.

--- scripts/create-test-site.sh
+++ scripts/create-test-site.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env sh
 
-drush site-install journal -y --site-name="Test Journal"
-drush user-password admin $1
-drush ucrt "Sample Submitter" --password=$1
-drush user:role:add "journal_article_creator" "Sample Submitter"
-drush ucrt "Sample Reviewer" --password=$1
-drush user:role:add "journal_article_reviewer" "Sample Reviewer"
-drush en devel_generate
-drush devel-generate-content 1 --types=journal
-drush devel-generate-content 2 --types=issue
+if drush site-install journal -y --site-name="Test Journal"; then
+    drush user-password admin $1
+    drush ucrt "Sample Submitter" --password=$1
+    drush user:role:add "journal_article_creator" "Sample Submitter"
+    drush ucrt "Sample Reviewer" --password=$1
+    drush user:role:add "journal_article_reviewer" "Sample Reviewer"
+    drush en devel_generate
+    drush devel-generate-content 1 --types=journal
+    drush devel-generate-content 2 --types=issue
+fi
